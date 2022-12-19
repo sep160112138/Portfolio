87633fd934c65ef535ffe466d36301c0342584b2
sep160112138 <chris.bo.han2009@gmail.com>
2022-12-20 00:38:53 +0800

    finished

--- src/assets/scripts/home.js
+++ src/assets/scripts/home.js
@@ -2,4 +2,26 @@ try {
     window.addEventListener("DOMContentLoaded", init);
 } catch (error) {};
 
-function init() {};
+function init() {
+    navigateHomeMenu();
+};
+
+function navigateHomeMenu() {
+    const aboutButton = document.querySelector('#about.home-menu');
+    const projectsButton = document.querySelector('#projects.home-menu');
+    const resumeButton = document.querySelector('#resume.home-menu')
+    const contactButton = document.querySelector('#contact.home-menu');
+
+    aboutButton.addEventListener('click', function() {
+        location.href = 'about.html';
+    })
+    projectsButton.addEventListener('click', function() {
+        location.href = 'projects.html';
+    });
+    resumeButton.addEventListener('click', function() {
+        location.href = 'resume.html'
+    });
+    contactButton.addEventListener('click', function() {
+        location.href = 'contact.html';
+    });
+}
